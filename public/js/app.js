8fdea8fa60011c6df0d3b21779f3cf40ed283360
Suryansh9142 <suryanshmishra003@gmail.com>
2024-08-03 21:16:24 +0530

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,42 @@
+console.log('Client side javascript file is loaded!')
+
+// 
+// Q) Goal: Reander content to paragraphs
+// 
+// 1. Select the secound message p from JavaScript
+// 2. Jsut befor fetch, render loading mesage and empty p
+// 3. If error, render error
+// 4. If no error,render location and forcast
+// 5. Test your work! Search for errors and for valid location
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+// messageOne.textContent = 'Form JavaScript'
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    fetch('http://127.0.0.1:3000/weather?address=' + location).then((responce) => {
+        responce.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                // messageTwo.textContent = data.forecast
+                messageTwo.textContent = Object.entries(data.forecast)
+                // .map(([key, value]) => `${key}: ${value}`)
+                // .join('\n');
+                console.log(data.location)
+                console.log(data.forecast)
+            }
+        })
+    })
+})
---
+++ src/utils.js
@@ -0,0 +1,53 @@
+const request = require('request')
+
+const geoCode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1Ijoic3VyeWFuc2gwMDMiLCJhIjoiY2x5ZnU1ZTlwMDNpbTJpcG9kd2h6ZWc4ZCJ9.vIbHkQ9MPV22men4RnWjQA&limit=1'
+
+    request({url: url,json: true}, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (body.features.length === 0) {
+            callback('Unable to find location. Try another search. ',undefined) 
+        } else {
+            callback(undefined, {
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
+            })
+        }
+    })
+}
+
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=e657c41dbcf2fab09409de61805bd167&query='+ latitude + ',' + longitude
+
+    console.log("Requesting Weatherstack API with URL:", url);
+
+    request({ url: url, json: true }, (error, { body }) => {
+        if (error) {
+            console.log("Error connecting to weather service:", error);
+            callback('Unable to connect to weather service!', undefined);
+        } else {
+            console.log("Weatherstack API response:", body);
+
+            if (body.error) {
+                console.log("Weatherstack API error:", body.error);
+                callback('Unable to find location. Try another search.', undefined);
+            } else {
+                const current = body.current;
+                callback(undefined, {
+                    description: current.weather_descriptions[0],
+                    temperature: current.temperature,
+                    feelsLike: current.feelslike,
+                    humidity: current.humidity
+                });
+            }
+        }
+    });
+}
+
+module.exports = {
+    geoCode: geoCode,
+    forecast: forecast
+}
